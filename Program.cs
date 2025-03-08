3a68bff6bcf64830f7c81102a76566b1a2a9cdc7
ETEC Adolpho Berezin <https://github.com/NICOLAS007O/Musica.git>
2025-03-07 22:12:42 -0300

    Exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine(@"
+Pó, pó, pó, pó, pó, pó
+Pó, pó, pó, pó, pó, pó
+");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"
+A galinha pintadinha
+E o galo carijó
+A galinha usa saia
+E o galo, paletó
+
+A galinha ficou doente
+E o galo nem ligou
+E os pintinhos foram correndo
+Pra chamar o seu doutor
+
+O doutor era o peru (glu, glu)
+E a enfermeira era um urubu
+E a agulha da injeção
+Era a pena de um pavão (ui!)
+");
+
+Console.ResetColor();
+Console.WriteLine(@"
+Pó, pó, pó, pó, pó
+Pó, pó, pó, pó, pó
+Pó, pó, pó, pó, pó
+Pó, pó, pó, pó, pó, pó, pó
+Pó, pó, pó, pó, pó
+Pó, pó, pó, pó, pó
+Pó, pó, pó, pó, pó, pó, pó
+");
